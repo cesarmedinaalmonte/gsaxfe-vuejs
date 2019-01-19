8e53080f951ce84b3a80effed3b3a29f0e5a1a11
Enercido Alcantara <ealcantara22@gmail.com>
2019-01-19 04:16:20 +0000

    logo app y server url

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const baseURL = 'http://gsax-app.local/app_dev.php/';
+// const baseURL = 'http://gsax-app.local/app_dev.php/';
+const baseURL = 'http://104.196.4.97:8000/app_dev.php';
+
+
 
 export default axios.create({
     baseURL: baseURL,
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,13 @@
 <template>
     <el-container style="background-color: #2d3a4b;">
-        <el-main style="margin-top: 100px; color: #fff">
+        <el-main style="margin-top: 40px; color: #fff">
             <el-row :gutter="20">
                 <el-col :span="6" :offset="9">
                     <div style="text-align: center; margin-bottom: 55px;">
 
                         <div class="login-header">
-                            <p style="font-size: 35px; margin-bottom: 0px; font-weigh: bold;">GSAX App</p>
+                            <!-- <p style="font-size: 35px; margin-bottom: 0px; font-weigh: bold;">GSAX App</p> -->
+                            <img src="@/assets/app_logo.png" alt="" style="width: 35%;">
                             <h3>Login</h3>
                         </div>
 
